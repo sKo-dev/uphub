f0acd8e064fce146fd0669f782a803d06ddc56c6
coolatbowlingYT <40440144+coolatbowlingYT@users.noreply.github.com>
2021-12-29 22:25:35 -0800

    Update source.lua

--- source.lua
+++ source.lua
@@ -2293,7 +2293,7 @@ local isSuc, info = pcall(market.GetProductInfo,market,game.PlaceId)
             end)
         
             GamePage3:AddButton("Epic Minigames", function()
-                loadstring(game:HttpGet("https://raw.githubusercontent.com/Eazvy/epic/main/epic"))()
+               loadstring(game:HttpGet("https://pastebin.com/raw/YePwz5u5", true))()
             end)
             --Game TP
 
