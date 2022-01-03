085278d597d198aa85e508db8a0138bb18214008
coolatbowlingYT <40440144+coolatbowlingYT@users.noreply.github.com>
2022-01-02 23:33:11 -0800

    scp3008

--- source.lua
+++ source.lua
@@ -2219,6 +2219,22 @@ local isSuc, info = pcall(market.GetProductInfo,market,game.PlaceId)
             GamePage3:AddButton("Zombie Rush", function()
                loadstring(game:HttpGet('https://raw.githubusercontent.com/1201for/V.G-Hub/main/V.Ghub',true))()
             end)
+            GamePage3:Button("scp3008",function()
+            local notifLib = loadstring(game:HttpGet(('https://gist.githubusercontent.com/lokk1y-dev/adbd2be45ba69c4ce71a39bcb4b8899a/raw/c1041fd9cff9b4e1ae5fdf619ef6582592d1a6f4/notifLib.lua'),true))()
+             notifLib.prompt('Notification', 'AntiFallDamage now is enabled!', 16)
+            local oldnc
+             oldnc = hookmetamethod(game,"__namecall",newcclosure(function(name, ...)
+            local Args = {...}
+             if getnamecallmethod() == "FireServer" and not checkcaller() and tostring(name) == "RemoteEvent" and type(Args[1]["OriginalDamage"]) == "number" then
+                Args[1]["OriginalDamage"] = 0
+                Args[1]["Softened"] = true
+                Args[1]["Broken"] = false
+                Args[1]["Range"] = 0
+                Args[1]["Damage"] = 0
+          end
+          return oldnc(name, unpack(Args))
+              end))
+              end)
             --Game TP
 
             local GameTpPage1 = GameTP:AddFolder("Page 1")
