6c78ee1b3622575db885ab3e052c0d6203a3bc4e
coolatbowlingYT <40440144+coolatbowlingYT@users.noreply.github.com>
2022-01-19 16:09:42 -0800

    Update source.lua

--- source.lua
+++ source.lua
@@ -2205,7 +2205,7 @@ local isSuc, info = pcall(market.GetProductInfo,market,game.PlaceId)
             end)
 
             GamePage3:AddButton("Tower of Hell", function()
-                loadstring(game:HttpGet("https://raw.githubusercontent.com/insanedude59/CheetoHub/main/1962086868",true))()
+                loadstring(game:HttpGet("https://raw.githubusercontent.com/insanedude59/CheetoHub/main/Loader",true))()
             end)
 		
             GamePage3:AddButton("Hexa Game (Squid Game)", function()
